e7c921e42f7d873a875d4b2bb5c9479b186b3280
aymen <aymenlabidi21@gmail.com>
2017-10-26 13:50:25 +0100

    v2

---
+++ app/src/main/java/com/example/root/mvpbooks/repositories/BooksRepository.java
@@ -0,0 +1,8 @@
+package com.example.root.mvpbooks.repositories;
+
+/**
+ * Created by root on 26/10/17.
+ */
+
+public interface BooksRepository {
+}
---
+++ app/src/test/java/com/example/root/mvpbooks/BooksActivityPresenterTest.java
@@ -0,0 +1,19 @@
+package com.example.root.mvpbooks;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by root on 26/10/17.
+ */
+public class BooksActivityPresenterTest {
+    @Test
+    public void shouldpass()
+    {
+        Assert.assertEquals(1,1);
+    }
+
+}
